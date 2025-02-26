a8b7684b4ee57406807ca698276f163bd1be072f
Talha Pakdil <talhapakdil7@gmail.com>
2025-02-26 12:17:53 +0300

    first exam

---
+++ Odev1/Odev1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Odev1;
+
+
+
+
+
+
+Bmw bmw = new Bmw();
+
+Porsche porsche = new Porsche();
+
+
+Togg togg = new Togg();
+
+
+bmw.vitesTur = "düz viteslidir";
+
+porsche.vitesTur = "otomatiktir";
+togg.vitesTur = "otmatiktir";
+
+
+
+Console.WriteLine(bmw.vitesTur);
+
+Console.WriteLine(porsche.vitesTur);    
+
+
+
+
