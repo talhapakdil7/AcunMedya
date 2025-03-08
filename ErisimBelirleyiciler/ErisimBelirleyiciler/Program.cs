593e1806660938b832d435fe9cb9b33df0f7ca97
Talha Pakdil <talhapakdil7@gmail.com>
2025-03-09 01:14:11 +0300

    erisimbelirleyici

---
+++ ErisimBelirleyiciler/ErisimBelirleyiciler/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ErisimBelirleyiciler;
+
+
+
+
+
+
+
+User user = new User(); 
+
+
+user.Name = "Ali";
+user.Surname = "Veli";
+user.Age = 25;
+user.email = "asdjna@gmail.com";
+
+user.BilgiGoster();
---
+++ ErisimBelirleyiciler/ErisimBelirleyiciler/User.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErisimBelirleyiciler
+{
+    internal class User
+    {
+
+        private string name;
+
+
+        private string surname;
+        private int age;
+        public string email;
+
+
+        public void bilgiGoster()
+        {
+
+        }
+
+        public string Name
+        {
+            get { return name; }
+
+            set { name = value.ToUpper(); }
+
+        }
+
+        public string Surname
+        {
+
+            get { return surname; }
+            set { surname = value.ToUpper(); }
+
+
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 0)
+                {
+
+                    age = value;
+
+                }
+                else
+                {
+
+                    Console.WriteLine("gecersiz yas");
+                }
+
+
+            }
+
+        }
+            public void BilgiGoster()
+        {
+            Console.WriteLine("Adi: " + name);
+            Console.WriteLine("Soyadi: " + surname);
+            Console.WriteLine("Yasi: " + age);
+            Console.WriteLine("Email: " + email);
+        }
+
+
+    }
+}
